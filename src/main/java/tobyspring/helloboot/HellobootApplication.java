7e52335d13aebba1e62afa95b4ed18e1e0fc949d
0082 <0082@coreit.co.kr>
2024-08-20 16:59:31 +0900

    디스패처를 빈으로 등록, 생명주기에 따른 소스변경

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,6 +13,16 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+	
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
@@ -22,11 +33,15 @@ public class HellobootApplication {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); 
+				
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				//dispatcherServlet.setApplicationContext(this); //이 소스는 필요없음. 디스패처서블릿이 넣어줌 
+				//lifecycle : (ApplicationContextAware - setApplicationContext(ApplicationContext applicationContext)
+				
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {		
-					servletContext.addServlet("dispatcherServlet", 
-								new DispatcherServlet(this))
-					.addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+								.addMapping("/*");
 				});
 				
 				webServer.start();

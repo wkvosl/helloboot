1b700f757e80bd9fb82752b8f58d8821f4000cb4
0082 <0082@coreit.co.kr>
2024-08-20 13:11:31 +0900

    서블릿 등록, 특정 url 응답 확인

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,7 +1,16 @@
 package tobyspring.helloboot;
 
+import java.io.IOException;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 //@SpringBootApplication
 public class HellobootApplication {
@@ -12,7 +21,35 @@ public class HellobootApplication {
 		//톰캣 시작 : 내장톰캣 불러오기
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 //		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //ServletWebServerFactory으로 받아도 됨.
-		WebServer webServer = serverFactory.getWebServer(); //서블릿 컨테이너를 만드는 생성함수.
+
+		
+		/*
+		//익명클래스 생성
+		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+
+			@Override
+			public void onStartup(ServletContext servletContext) throws ServletException {
+				
+			}
+			
+		}); //서블릿 컨테이너를 만드는 생성함수.
+		*/
+		
+		//위 소스가 람다식으로 변경됨. 단축키 Ctrl + 1
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+
+				protected void service(HttpServletRequest req, HttpServletResponse resp)
+						throws ServletException, IOException {
+					// TODO Auto-generated method stub
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().print("Hello Servlet");
+				}
+				
+			}).addMapping("/hello");
+		}); //서블릿 컨테이너를 만드는 생성함수.
+		
 		webServer.start();
 		
 	}

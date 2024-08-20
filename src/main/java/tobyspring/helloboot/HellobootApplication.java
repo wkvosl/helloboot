3388da60b35dfe60a5838b85c86af9b29d78cbe2
0082 <0082@coreit.co.kr>
2024-08-20 11:37:10 +0900

    어노테이션, run() 없이 서블릿 톰캣 띄우기 (내장톰캣)

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,20 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+//		SpringApplication.run(HellobootApplication.class, args);
+		
+		//톰캣 시작 : 내장톰캣 불러오기
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //ServletWebServerFactory으로 받아도 됨.
+		WebServer webServer = serverFactory.getWebServer(); //서블릿 컨테이너를 만드는 생성함수.
+		webServer.start();
+		
 	}
 
 }

f0bfcd0d247bd14cbb5f4db737d4baf8bc509427
0082 <0082@coreit.co.kr>
2024-08-20 16:14:40 +0900

    자바로 구성해보기 (빈 팩토리)

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,30 +3,49 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 //@SpringBootApplication
+@Configuration
 public class HellobootApplication {
 
+	//팩토리메서드
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+	
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService(); //인터페이스타입으로 생성
+	}
+	
+	
 	public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
 		
 		//스프링컨테이너
-		GenericWebApplicationContext appCon = new GenericWebApplicationContext();
-		appCon.registerBean(HelloController.class); //bean 등록 끝.
-		appCon.registerBean(SimpleHelloService.class); //bean 등록 끝.
+		AnnotationConfigWebApplicationContext appCon = new AnnotationConfigWebApplicationContext() {
+			
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); 
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {		
+					servletContext.addServlet("dispatcherServlet", 
+								new DispatcherServlet(this))
+					.addMapping("/*");
+				});
+				
+				webServer.start();
+			}
+		};
+
+		appCon.register(HellobootApplication.class);
 		appCon.refresh(); //초기화 : 빈 오브젝트 생성
-		
-		
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); 
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {		
-			servletContext.addServlet("dispatcherServlet", 
-						new DispatcherServlet(appCon)
-					).addMapping("/*");
-		});
-		
-		webServer.start();
 		
 	}
 

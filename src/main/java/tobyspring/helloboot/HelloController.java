bba67ae3568051d1f0c6e819d0ee7314aab8f5da
0082 <0082@coreit.co.kr>
2024-08-20 15:51:21 +0900

    서블릿 디스패쳐로 매핑

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,6 +2,14 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
 public class HelloController {
 
 	private HelloService helloService;
@@ -10,7 +18,9 @@ public class HelloController {
 		this.helloService = helloService;
 	}
 	
-	public String hello(String name){
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello(@RequestParam("name") String name){
 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
 	
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,18 +1,10 @@
 package tobyspring.helloboot;
 
-import java.io.IOException;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 //@SpringBootApplication
 public class HellobootApplication {
@@ -21,40 +13,18 @@ public class HellobootApplication {
 //		SpringApplication.run(HellobootApplication.class, args);
 		
 		//스프링컨테이너
-		GenericApplicationContext appCon = new GenericApplicationContext();
+		GenericWebApplicationContext appCon = new GenericWebApplicationContext();
 		appCon.registerBean(HelloController.class); //bean 등록 끝.
 		appCon.registerBean(SimpleHelloService.class); //bean 등록 끝.
 		appCon.refresh(); //초기화 : 빈 오브젝트 생성
 		
 		
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); 
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); 
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {		
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					
-					// 인증, 보안, 다국어, 공통 기능을 만들고
-					
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-						String name = req.getParameter("name");
-						
-						HelloController helloController = appCon.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-						
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().print("Hello Servlet : " + ret);
-					}
-					
-					else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-					
-				}
-				
-			}).addMapping("/*");
-		}); 
+			servletContext.addServlet("dispatcherServlet", 
+						new DispatcherServlet(appCon)
+					).addMapping("/*");
+		});
 		
 		webServer.start();
 		
